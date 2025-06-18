45e66a1d778cd38139722964f236a992839fbf83
kraanzu <kraanzu@gmail.com>
2025-06-18 19:42:46 +0530

    Add tcss highlights

---
+++ queries/highlights.scm
@@ -0,0 +1,44 @@
+;; Comments
+(comment) @comment
+
+;; Selectors
+(type_selector) @type
+(class_selector) @class
+(id_selector) @constant
+(pseudo_class_selector) @function
+(wildcard_selector) @type
+(parent_reference) @keyword
+
+;; Properties and declarations
+(property_name) @property
+(declaration ":") @punctuation.delimiter
+(important) @keyword
+
+;; Values
+(value) @string
+(value_list) @string
+
+;; Variables
+(variable) @variable
+(variable_reference) @variable
+
+;; Functions and function arguments
+(function_call) @function
+(function_call "(") @punctuation.bracket
+(function_call ")") @punctuation.bracket
+
+;; Rule blocks
+(rule_set "{" @punctuation.bracket
+(rule_set "}") @punctuation.bracket
+
+;; Variable declaration
+(variable_declaration ":") @punctuation.delimiter
+(variable_declaration ";") @punctuation.delimiter
+
+;; Commas and separators
+(selector_group "," @punctuation.delimiter
+(value_list "," @punctuation.delimiter
+
+;; Braces and combinators
+(">" @operator)
+(";" @punctuation.delimiter)
